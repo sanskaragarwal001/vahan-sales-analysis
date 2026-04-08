5635a9878b9b003d9fe6e3ca6fd11e52747499eb
Sanskar Agarwal <aggrawalsanskar@gmail.com>
2026-04-08 19:47:27 +0530

    Add BrowserManager class and config files for Playwright automation

---
+++ src/browser.ts
@@ -0,0 +1,50 @@
+import { chromium } from "playwright";
+import type { Browser, Page } from "playwright";
+
+import { browserLauchOptions, newPageOptions } from "./config.ts";
+
+export class BrowserManager {
+  private static browser: Browser | null = null;
+
+  private constructor() {}
+
+  public static async initializeBrowser(): Promise<void> {
+    if (BrowserManager.browser) {
+      return;
+    }
+
+    try {
+      BrowserManager.browser = await chromium.launch(browserLauchOptions);
+    } catch (error) {
+      console.error("Browser initialization failed");
+      throw error;
+    }
+  }
+
+  public static async closeBrowser(): Promise<void> {
+    if (BrowserManager.browser == null) {
+      return;
+    }
+
+    try {
+      await BrowserManager.browser.close();
+      BrowserManager.browser = null;
+    } catch (error) {
+      console.error("Browser cannot be closed");
+      throw error;
+    }
+  }
+
+  public static async getNewPage(): Promise<Page> {
+    if (BrowserManager.browser == null) {
+      throw new Error("Browser not initialized");
+    }
+
+    try {
+      return await BrowserManager.browser.newPage(newPageOptions);
+    } catch (error) {
+      console.error("Error creating new page");
+      throw error;
+    }
+  }
+}
---
+++ src/config.ts
@@ -0,0 +1,14 @@
+import type { LaunchOptions } from "playwright";
+
+export const vahanDashboardUrl =
+  "https://vahan.parivahan.gov.in/vahan4dashboard/vahan/view/reportview.xhtml";
+
+export const browserLauchOptions: LaunchOptions = {
+  headless: false,
+  slowMo: 200,
+  args: [],
+};
+
+export const newPageOptions = {
+  acceptDownloads: true,
+};
